0ea1002a370f8a533b600a88b1d5106f8b761a45
Luca Ramundo <0xc14m1z@gmail.com>
2019-07-16 08:08:20 +0200

    Add #isUsernameAvailable method and bind the socket and the client to the message handler

--- src/server/index.js
+++ src/server/index.js
@@ -8,8 +8,13 @@ const socket = new WebSocket.Server({
   port: process.env.PORT || 8888
 })
 
+socket.isUsernameAvailable = function(username) {
+  return !Array.from(this.clients)
+               .find(client => client.username === username)
+}
+
 socket.on('connection', client => {
   client.on('close', onClose)
-  client.on('message', onMessage)
+  client.on('message', onMessage.bind(socket, client))
   client.on('error', onError)
 })
