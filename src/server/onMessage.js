433e5f293384d35d46981671b747e5a474635d6d
Luca Ramundo <0xc14m1z@gmail.com>
2019-07-16 08:11:37 +0200

    Refactor onMessage handler to route to proper handler reading the map

--- src/server/onMessage.js
+++ src/server/onMessage.js
@@ -1,6 +1,15 @@
-function onMessage(rawMessage) {
+const decodeMessage = require('../common/decodeMessage')
+const handlersMap = require('./handlersMap')
+
+function onMessage(client, rawMessage) {
   console.log('Got a new message:')
   console.log(rawMessage)
+
+  const { type, payload } = decodeMessage(rawMessage)
+
+  if ( type in handlersMap ) {
+    handlersMap[type].call(this, client, payload)
+  }
 }
 
 module.exports = onMessage
