a51020b9fb4c32774a2430c64deb08ed5ac675ac
0xc14m1z <0xc14m1z@gmail.com>
2019-07-19 11:10:44 +0200

    Add tests and implementation for Signal#openConnection

--- src/client/Signal.js
+++ src/client/Signal.js
@@ -1,6 +1,21 @@
+const decodeMessage = require('../common/decodeMessage')
+
+const Socket = {
+  OPEN: 'open',
+  CLOSE: 'close',
+  ERROR: 'error',
+  MESSAGE: 'message'
+}
+
 function Signal() {
+  this.socket = null
+  this.isConnected = false
   this.eventHandlers = {}
 }
+
+Signal.OPEN = 'OPEN'
+Signal.CLOSE = 'CLOSE'
+Signal.ERROR = 'ERROR'
 
 Signal.prototype.on = function on(event, handler) {
   if ( !this.eventHandlers[event] ) this.eventHandlers[event] = []
@@ -12,6 +27,36 @@ Signal.prototype.emit = function emit(event, ...payload) {
   handlers.forEach(function run(handler) {
     if ( typeof handler === 'function' ) handler.call(this, ...payload)
   })
+}
+
+Signal.prototype.openConnection = function openConnection(url) {
+  this.socket = new WebSocket(url)
+
+  function onOpen() {
+    this.isConnected = true
+    this.emit(Signal.OPEN)
+  }
+
+  function onClose() {
+    this.socket = null
+    this.isConnected = false
+    this.emit(Signal.CLOSE)
+  }
+
+  function onError(error) {
+    this.isConnected = false
+    this.emit(Signal.ERROR)
+  }
+
+  function onMessage(rawMessage) {
+    const { type, payload } = decodeMessage(rawMessage.data)
+    this.emit(type, payload)
+  }
+
+  this.socket.addEventListener(Socket.OPEN, onOpen.bind(this))
+  this.socket.addEventListener(Socket.CLOSE, onClose.bind(this))
+  this.socket.addEventListener(Socket.ERROR, onError.bind(this))
+  this.socket.addEventListener(Socket.MESSAGE, onMessage.bind(this))
 }
 
 module.exports = Signal
--- src/client/__tests__/Signal.js
+++ src/client/__tests__/Signal.js
@@ -2,6 +2,9 @@ const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+const { WebSocket, Server } = require('mock-socket')
+
+global.WebSocket = WebSocket
 
 chai.use(sinonChai)
 
@@ -61,11 +64,94 @@ describe('Signal', () => {
       expect(secondHandler).to.have.been.called
     })
 
+    it('skips non-function handlers', () => {
+      const handler = 42
+      signal.on('event', handler)
+
+      const emission = () => signal.emit('event')
+      expect(emission).to.not.throw()
+    })
+
   })
 
   describe('openConnection', () => {
-    it('connects to an existing signaling server')
-    it('throws an error whether the signaling server doesn\'t exist')
+
+    const signalingUrl = 'ws://signaling:12345'
+    let server
+
+    beforeEach(() => {
+      server = new Server(signalingUrl)
+    })
+
+    afterEach(() => {
+      server.stop()
+    })
+
+    it('connects to a signaling server and emits the Signal.OPEN event', done => {
+      const openHandler = sinon.spy()
+
+      signal.on(Signal.OPEN, openHandler)
+      signal.on(Signal.OPEN, assertions)
+      signal.openConnection(signalingUrl)
+
+      function assertions() {
+        expect(openHandler).to.have.been.called
+        expect(signal.socket).to.not.equal(null)
+        expect(signal.isConnected).to.be.true
+        done()
+      }
+    })
+
+    it('emits the Signal.CLOSE event when connection is closed', done => {
+      const closeHandler = sinon.spy()
+
+      signal.on(Signal.CLOSE, closeHandler)
+      signal.on(Signal.CLOSE, assertions)
+      signal.openConnection(signalingUrl)
+
+      server.on('connection', client => client.close())
+
+      function assertions() {
+        expect(closeHandler).to.have.been.called
+        expect(signal.socket).to.equal(null)
+        expect(signal.isConnected).to.be.false
+        done()
+      }
+    })
+
+    it('emits the Signal.ERROR event when something wrong happen', done => {
+      const errorHandler = sinon.spy()
+
+      signal.on(Signal.ERROR, errorHandler)
+      signal.on(Signal.ERROR, assertions)
+      signal.openConnection(signalingUrl)
+
+      server.simulate('error')
+
+      function assertions() {
+        expect(errorHandler).to.have.been.called
+        expect(signal.isConnected).to.be.false
+        done()
+      }
+    })
+
+    it('emits a custom event based on incoming message "type" property and pass the payload', done => {
+      const messageHandler = sinon.spy()
+      const message = '{ "type": "custom-event", "payload": [1, 2, 3] }'
+
+      signal.on('custom-event', messageHandler)
+      signal.on('custom-event', assertions)
+      signal.openConnection(signalingUrl)
+
+      server.on('connection', client => client.send(message))
+
+      function assertions() {
+        expect(messageHandler).to.have.been.called
+        expect(messageHandler).to.have.been.calledWith([1, 2, 3])
+        done()
+      }
+    })
+
   })
 
 })
