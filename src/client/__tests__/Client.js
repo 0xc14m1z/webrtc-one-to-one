a131931387778943778e2a60af18394e82c26104
Luca Ramundo <0xc14m1z@gmail.com>
2019-07-20 19:24:11 +0200

    Draft tests for Client object

---
+++ src/client/__tests__/Client.js
@@ -0,0 +1,24 @@
+const chai = require('chai')
+const expect = chai.expect
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai)
+
+const Client = require('../Client')
+
+describe('Client', () => {
+
+  describe('constructor', () => {})
+
+  describe('connectAs', () => {})
+
+  describe('setLocalStream', () => {})
+
+  describe('requestCall', () => {})
+
+  describe('acceptCall', () => {})
+
+  describe('rejectCall', () => {})
+
+})
