e1b5250fcc1dc283e96ebaaa4889253b8b0c5f4f
Luca Ramundo <0xc14m1z@gmail.com>
2019-07-23 09:26:04 +0200

    Add tests and implementation for Client constructor

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,3 +1,54 @@
-function Client() {}
+const EventsEmitter = require('./EventsEmitter')
+const Signal = require('./Signal')
+
+const MessageType = require('../common/messageTypes')
+
+const defaultOptions = {
+  iceServers: [{
+    urls: ['stun:stun.l.google.com:19302']
+  }]
+}
+
+function Client(url, options) {
+  EventsEmitter.call(this)
+
+  this.options = options || defaultOptions
+
+  this.connection = null
+  this.stream = null
+
+  this.signal = null
+  setupSignal.call(this)
+  this.signal.openConnection(url)
+}
+
+Client.STREAM = 'STREAM'
+
+function bubbleUp(event, payload) {
+  this.emit(event, payload)
+}
+
+function setupSignal() {
+  this.signal = new Signal()
+
+  const eventsToBubble = [
+    Signal.OPEN,
+    Signal.CLOSE,
+    Signal.ERROR,
+
+    MessageType.USERNAME_TAKEN,
+    MessageType.CONNECTED,
+
+    MessageType.UNKNOWN_RECIPIENT,
+    MessageType.CALL_REQUESTED,
+    MessageType.CALL_REJECTED
+  ]
+
+  eventsToBubble.forEach(
+    event => this.signal.on(event, bubbleUp.bind(this, event))
+  )
+}
+
+Client.prototype = Object.create(EventsEmitter.prototype)
 
 module.exports = Client
--- src/client/__tests__/Client.js
+++ src/client/__tests__/Client.js
@@ -2,14 +2,94 @@ const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
 const sinonChai = require('sinon-chai')
+const { WebSocket, Server } = require('mock-socket')
+
+global.WebSocket = WebSocket
 
 chai.use(sinonChai)
 
+const Signal = require('../Signal')
 const Client = require('../Client')
+const MessageType = require('../../common/messageTypes')
 
 describe('Client', () => {
 
-  describe('constructor', () => {})
+  const signalingUrl = 'ws://signaling:12345'
+  let client
+  let server
+
+  beforeEach(() => {
+    server = new Server(signalingUrl)
+    client = new Client(signalingUrl)
+  })
+
+  afterEach(() => {
+    server.stop()
+  })
+
+  describe('constructor', () => {
+
+    it('initialize the events emitter', () => {
+      expect(client.handlers).to.be.an('object')
+    })
+
+    it('sets custom options if provided', () => {
+      const defaultOptions = {
+        iceServers: [{
+          urls: ['stun:stun.l.google.com:19302']
+        }]
+      }
+
+      expect(client.options).to.deep.equal(defaultOptions)
+
+      client = new Client(signalingUrl, 'fake custom options')
+      expect(client.options).to.deep.equal('fake custom options')
+    })
+
+    it('initialize the signaling object', () => {
+      expect(client.signal).to.be.an.instanceof(Signal)
+
+      const registeredEvents = [
+        Signal.OPEN,
+        Signal.CLOSE,
+        Signal.ERROR,
+
+        MessageType.USERNAME_TAKEN,
+        MessageType.CONNECTED,
+
+        MessageType.UNKNOWN_RECIPIENT,
+        MessageType.CALL_REQUESTED,
+        MessageType.CALL_REJECTED
+      ]
+      expect(client.signal.handlers).to.have.keys(registeredEvents)
+    })
+
+    it('sets events that bubbles up', () => {
+      const handler = sinon.spy()
+
+      const eventsToBubble = [
+        Signal.OPEN,
+        Signal.CLOSE,
+        Signal.ERROR,
+
+        MessageType.USERNAME_TAKEN,
+        MessageType.CONNECTED,
+
+        MessageType.UNKNOWN_RECIPIENT,
+        MessageType.CALL_REQUESTED,
+        MessageType.CALL_REJECTED
+      ]
+
+      // add event handlers to the client
+      eventsToBubble.forEach(event => client.on(event, handler))
+
+      // manually emits events from the signal object
+      eventsToBubble.forEach(event => client.signal.emit(event))
+
+      expect(handler.callCount).to.equal(eventsToBubble.length)
+    })
+
+  })
 
   describe('connectAs', () => {})
 
