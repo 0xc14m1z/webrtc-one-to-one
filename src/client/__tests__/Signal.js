196646f9f24a45d2699fd12c496bed6e28ac9a85
Luca Ramundo <0xc14m1z@gmail.com>
2019-07-24 23:17:43 +0200

    Add tests for outbound messages

--- src/client/__tests__/Signal.js
+++ src/client/__tests__/Signal.js
@@ -10,25 +10,25 @@ chai.use(sinonChai)
 
 const Signal = require('../Signal')
 
+const decodeMessage = require('../../common/decodeMessage')
+const MessageType = require('../../common/messageTypes')
+
 describe('Signal', () => {
 
   const signalingUrl = 'ws://signaling:12345'
+  let server
   let signal
 
   beforeEach(() => {
+    server = new Server(signalingUrl)
     signal = new Signal(signalingUrl)
   })
 
+  afterEach(() => {
+    server.stop()
+  })
+
   describe('openConnection', () => {
-    let server
-
-    beforeEach(() => {
-      server = new Server(signalingUrl)
-    })
-
-    afterEach(() => {
-      server.stop()
-    })
 
     it('connects to a signaling server and emits the Signal.OPEN event', done => {
       const openHandler = sinon.spy()
@@ -91,6 +91,137 @@ describe('Signal', () => {
       function assertions() {
         expect(messageHandler).to.have.been.called
         expect(messageHandler).to.have.been.calledWith([1, 2, 3])
+        done()
+      }
+    })
+
+  })
+
+  describe('Outbound messages', () => {
+
+    it('connectAs', done => {
+      const username = 'luke skywalker'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(Signal.OPEN, () => signal.connectAs(username))
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.CONNECT)
+        expect(payload).to.equal(username)
+        done()
+      }
+    })
+
+    it('requestCall', done => {
+      const recipient = 'luke skywalker'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(Signal.OPEN, () => signal.requestCall(recipient))
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.REQUEST_CALL)
+        expect(payload).to.equal(recipient)
+        done()
+      }
+    })
+
+    it('acceptCall', done => {
+      const caller = 'luke skywalker'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(Signal.OPEN, () => signal.acceptCall(caller))
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.ACCEPT_CALL)
+        expect(payload).to.equal(caller)
+        done()
+      }
+    })
+
+    it('rejectCall', done => {
+      const caller = 'luke skywalker'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(Signal.OPEN, () => signal.rejectCall(caller))
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.REJECT_CALL)
+        expect(payload).to.equal(caller)
+        done()
+      }
+    })
+
+    it('sendCallerDescriptor', done => {
+      const recipient = 'luke skywalker'
+      const descriptor = 'sdp details'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(
+        Signal.OPEN,
+        () => signal.sendCallerDescriptor(recipient, descriptor)
+      )
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.SEND_CALLER_DESCRIPTOR)
+        expect(payload.to).to.equal(recipient)
+        expect(payload.sdp).to.equal(descriptor)
+        done()
+      }
+    })
+
+    it('sendRecipientDescriptor', done => {
+      const caller = 'luke skywalker'
+      const descriptor = 'sdp details'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(
+        Signal.OPEN,
+        () => signal.sendRecipientDescriptor(caller, descriptor)
+      )
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.SEND_RECIPIENT_DESCRIPTOR)
+        expect(payload.to).to.equal(caller)
+        expect(payload.sdp).to.equal(descriptor)
+        done()
+      }
+    })
+
+    it('sendICECandidate', done => {
+      const recipient = 'luke skywalker'
+      const candidate = 'ice candidate details'
+
+      server.on('connection', client => client.on('message', assertions))
+
+      signal.on(
+        Signal.OPEN,
+        () => signal.sendICECandidate(recipient, candidate)
+      )
+      signal.openConnection()
+
+      function assertions(rawMessage) {
+        const { type, payload } = decodeMessage(rawMessage)
+        expect(type).to.equal(MessageType.SEND_ICE_CANDIDATE)
+        expect(payload.to).to.equal(recipient)
+        expect(payload.candidate).to.equal(candidate)
         done()
       }
     })
