df04d6456c529f03a8c48449fa34393939052b1a
0xc14m1z <0xc14m1z@gmail.com>
2019-08-06 12:51:05 +0200

    Add server messages handlers

--- src/server/handlersMap.js
+++ src/server/handlersMap.js
@@ -5,6 +5,8 @@ module.exports = {
   [MessageType.CONNECT]: Handlers.onConnect,
   [MessageType.REQUEST_CALL]: Handlers.onRequestCall,
   [MessageType.ACCEPT_CALL]: Handlers.onAcceptCall,
-  [MessageType.REJECT_CALL]: Handlers.onRejectCall
-  [MessageType.SEND_CALLER_DESCRIPTOR]: Handlers.onSendCallerDescriptor
+  [MessageType.REJECT_CALL]: Handlers.onRejectCall,
+  [MessageType.SEND_CALLER_DESCRIPTOR]: Handlers.onSendCallerDescriptor,
+  [MessageType.SEND_RECIPIENT_DESCRIPTOR]: Handlers.onSendRecipientDescriptor,
+  [MessageType.SEND_ICE_CANDIDATE]: Handlers.onSendICECandidate
 }
