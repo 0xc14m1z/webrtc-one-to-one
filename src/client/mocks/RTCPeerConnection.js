cc90f89f1443258aafafb4c0985e1a405a172962
0xc14m1z <0xc14m1z@gmail.com>
2019-08-07 12:15:12 +0200

    Refine RTCPeerConnection mock

--- src/client/mocks/RTCPeerConnection.js
+++ src/client/mocks/RTCPeerConnection.js
@@ -1,18 +1,27 @@
-function noOp() {}
-function promiseNoOp() { return Promise.resolve() }
+const sinon = require('sinon')
 
 function RTCPeerConnection() {
   return {
-    onicecandidate: noOp,
-    ontrack: noOp,
-    addTrack: noOp,
-    setLocalDescription: promiseNoOp,
-    setRemoteDescription: promiseNoOp,
-    localDescription: null,
-    addIceCandidate: noOp,
-    createOffer: promiseNoOp,
-    createAnswer: promiseNoOp
+    onicecandidate: RTCPeerConnection.onicecandidate,
+    ontrack: RTCPeerConnection.ontrack,
+    addTrack: RTCPeerConnection.addTrack,
+    setLocalDescription: RTCPeerConnection.setLocalDescription,
+    setRemoteDescription: RTCPeerConnection.setRemoteDescription,
+    localDescription: RTCPeerConnection.localDescription,
+    addIceCandidate: RTCPeerConnection.addIceCandidate,
+    createOffer: RTCPeerConnection.createOffer,
+    createAnswer: RTCPeerConnection.createAnswer
   }
 }
+
+RTCPeerConnection.icecandidate = sinon.spy()
+RTCPeerConnection.track = sinon.spy()
+RTCPeerConnection.addTrack = sinon.spy()
+RTCPeerConnection.setLocalDescription = sinon.stub().resolves()
+RTCPeerConnection.setRemoteDescription = sinon.stub().resolves()
+RTCPeerConnection.localDescription = sinon.spy()
+RTCPeerConnection.addIceCandidate = sinon.spy()
+RTCPeerConnection.createOffer = sinon.stub().resolves()
+RTCPeerConnection.createAnswer = sinon.stub().resolves()
 
 module.exports = RTCPeerConnection
