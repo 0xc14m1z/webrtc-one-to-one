7e50b6ae39d0794e34748a032b6698ef72f41b41
0xc14m1z <0xc14m1z@gmail.com>
2019-08-07 12:43:14 +0200

    Add RTCSessionDescription mock

---
+++ src/client/mocks/RTCSessionDescription.js
@@ -0,0 +1,5 @@
+function RTCSessionDescription() {
+  return {}
+}
+
+module.exports = RTCSessionDescription
--- src/client/mocks/index.js
+++ src/client/mocks/index.js
@@ -1,7 +1,9 @@
 const RTCPeerConnection = require('./RTCPeerConnection')
 const MediaStream = require('./MediaStream')
+const RTCSessionDescription = require('./RTCSessionDescription')
 
 module.exports = {
   RTCPeerConnection,
-  MediaStream
+  MediaStream,
+  RTCSessionDescription
 }
